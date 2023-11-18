fa8bb71fffb8cd560167b908c3622e31fb721a8e
Lucas Bärlocher <lucas.baerlocher@olai.com>
2023-11-18 21:03:14 +0100

    Add redirect Cors rule to netlify

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function sendPrompt(message: string) {
   try {
     const response = await axios.post(
-      "https://b-bot-incubator.appengine.flow.ch/expert/askInternAgent",
+      "/api/expert/askBlackSanta",
       { prompt: message },
       {
         headers: {
