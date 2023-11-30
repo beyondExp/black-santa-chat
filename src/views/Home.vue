3932e387fa9c6fbaa6a9114a55dae8075f53a396
Lucas Bärlocher <lucas.baerlocher@olai.com>
2023-11-30 16:40:35 +0100

    adding snow effect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import ChatBox from '@/components/ChatBox.vue';
-import { VSnowDirective } from 'vsnow'
+
 </script>
 
 <template>
   <section class="flex justify-center mt-6">
     <div class="md:w-[35vw] mb-2" v-snow>
       <ChatBox />
+
     </div>
   </section>
 </template>
+<script>
+import { VSnow } from 'vsnow';
+
+export default {
+  components: {
+    VSnow
+  }
+};
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VSnow from 'vsnow'; // Importing v-snow
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VSnow,
   ],
   resolve: {
     alias: {
