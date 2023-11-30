5267b840204c727fa93a0bae03a4eb3f8bc8f4ae
Lucas Bärlocher <lucas.baerlocher@olai.com>
2023-11-30 20:15:13 +0100

    add tabs in modal

--- src/components/CookieModal.vue
+++ src/components/CookieModal.vue
@@ -5,9 +5,14 @@ import { CookieIcon } from 'lucide-vue-next'
 import { AvatarImage } from 'radix-vue'
 
 const showModal = ref(false)
+const activeTab = ref('monthly') // 'monthly' or 'one-time'
 
 const toggleModal = () => {
   showModal.value = !showModal.value;
+}
+
+const setActiveTab = (tab) => {
+  activeTab.value = tab;
 }
 
 const goToProduct = (url, productName) => {
@@ -25,40 +30,76 @@ const goToProduct = (url, productName) => {
     <div class="modal-content">
       <h1>Hinterlasse dem Santa einen Keks</h1>
       <p>Wähle aus, wie viele Kekse du dem Santa geben möchtest.</p>
+      <div class="tab-buttons">
+        <Button variant="ghost" :class="{ active: activeTab === 'monthly' }" @click="setActiveTab('monthly')">Monatlich</Button>
+        <Button variant="ghost" :class="{ active: activeTab === 'one-time' }" @click="setActiveTab('one-time')">Einmalig</Button>
+      </div>
+      <div v-if="activeTab === 'monthly'" class="tab-content">
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/aEUeYm8bwgehfFS8wz', '1 Keks')">
+          <AvatarImage class='card-image' src="/keks.png" alt="Santa" />
+          <div class="card-text">
+            <p>1 Keks</p>
+            <p>Preis: $1</p>
+          </div>
+        </div>
 
-      <div class="card" @click="() => goToProduct('https://buy.stripe.com/aEUeYm8bwgehfFS8wz', '1 Keks')">
-        <AvatarImage class='card-image' src="/keks.png" alt="Santa" />
-        <div class="card-text">
-          <p>1 Keks</p>
-          <p>Preis: $1</p>
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/5kAeYmgI2gehaly146', '2 Kekse')">
+          <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
+          <div class="card-text">
+            <p>2 Kekse</p>
+            <p>Preis: $2</p>
+          </div>
+        </div>
+
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/4gwdUibnI5zD79mdQR', '1 Keks und eine Milch')">
+          <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
+          <div class="card-text">
+            <p>1 Keks und eine Milch</p>
+            <p>Preis: $5</p>
+          </div>
+        </div>
+
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/bIY8zY63oe691P2aEE', 'Alles!')">
+          <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
+          <div class="card-text">
+            <p>Alles!</p>
+            <p>Preis: $8</p>
+          </div>
         </div>
       </div>
+      <div v-if="activeTab === 'one-time'" class="tab-content">
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/aEUeYm8bwgehfFS8wz', '1 Keks')">
+          <AvatarImage class='card-image' src="/keks.png" alt="Santa" />
+          <div class="card-text">
+            <p>1 Keks</p>
+            <p>Preis: $1</p>
+          </div>
+        </div>
 
-      <div class="card" @click="() => goToProduct('https://buy.stripe.com/5kAeYmgI2gehaly146', '2 Kekse')">
-        <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
-        <div class="card-text">
-          <p>2 Kekse</p>
-          <p>Preis: $2</p>
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/5kAeYmgI2gehaly146', '2 Kekse')">
+          <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
+          <div class="card-text">
+            <p>2 Kekse</p>
+            <p>Preis: $2</p>
+          </div>
+        </div>
+
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/4gwdUibnI5zD79mdQR', '1 Keks und eine Milch')">
+          <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
+          <div class="card-text">
+            <p>1 Keks und eine Milch</p>
+            <p>Preis: $5</p>
+          </div>
+        </div>
+
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/bIY8zY63oe691P2aEE', 'Alles!')">
+          <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
+          <div class="card-text">
+            <p>Alles!</p>
+            <p>Preis: $8</p>
+          </div>
         </div>
       </div>
-
-      <div class="card" @click="() => goToProduct('https://buy.stripe.com/4gwdUibnI5zD79mdQR', '1 Keks und eine Milch')">
-        <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
-        <div class="card-text">
-          <p>1 Keks und eine Milch</p>
-          <p>Preis: $5</p>
-        </div>
-      </div>
-
-      <div class="card" @click="() => goToProduct('https://buy.stripe.com/bIY8zY63oe691P2aEE', 'Alles!')">
-        <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
-        <div class="card-text">
-          <p>Alles!</p>
-          <p>Preis: $8</p>
-        </div>
-      </div>
-
-      <!-- Other cards -->
 
       <Button variant="secondary" @click="toggleModal">Close</Button>
     </div>
@@ -93,8 +134,17 @@ const goToProduct = (url, productName) => {
   overflow-y: auto; /* Enables vertical scrolling */
 }
 
+.tab-buttons .Button {
+  margin: 0 10px;
+}
+
+.active {
+  background-color: #007bff; /* Or your active tab color */
+  color: white;
+}
+
 .card {
-  background: var(--foreground);
+  background: hsl(var(--primary-foreground));
   border: 1px solid var(--card);
   border-radius: 10px;
   padding: 10px;
