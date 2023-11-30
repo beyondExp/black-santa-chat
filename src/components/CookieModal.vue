5e586afb3d6807bb67d769839329aa35ada368df
Lucas Bärlocher <lucas.baerlocher@olai.com>
2023-11-30 21:03:00 +0100

    adding style to modal

--- src/components/CookieModal.vue
+++ src/components/CookieModal.vue
@@ -31,15 +31,15 @@ const goToProduct = (url, productName) => {
       <h1>Hinterlasse dem Santa einen Keks</h1>
       <p>Wähle aus, wie viele Kekse du dem Santa geben möchtest.</p>
       <div class="tab-buttons">
-        <Button variant="ghost" :class="{ active: activeTab === 'monthly' }" @click="setActiveTab('monthly')">Monatlich</Button>
-        <Button variant="ghost" :class="{ active: activeTab === 'one-time' }" @click="setActiveTab('one-time')">Einmalig</Button>
+        <Button class='btn-switch' variant="ghost" :class="{ active: activeTab === 'monthly' }" @click="setActiveTab('monthly')">Monatlich</Button>
+        <Button class='btn-switch' variant="ghost" :class="{ active: activeTab === 'one-time' }" @click="setActiveTab('one-time')">Einmalig</Button>
       </div>
       <div v-if="activeTab === 'monthly'" class="tab-content">
         <div class="card" @click="() => goToProduct('https://buy.stripe.com/aEUeYm8bwgehfFS8wz', '1 Keks')">
           <AvatarImage class='card-image' src="/keks.png" alt="Santa" />
           <div class="card-text">
             <p>1 Keks</p>
-            <p>Preis: $1</p>
+            <p>Preis: 1.-</p>
           </div>
         </div>
 
@@ -47,7 +47,7 @@ const goToProduct = (url, productName) => {
           <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
           <div class="card-text">
             <p>2 Kekse</p>
-            <p>Preis: $2</p>
+            <p>Preis: 2.-</p>
           </div>
         </div>
 
@@ -55,48 +55,33 @@ const goToProduct = (url, productName) => {
           <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
           <div class="card-text">
             <p>1 Keks und eine Milch</p>
-            <p>Preis: $5</p>
+            <p>Preis: 5.-</p>
           </div>
         </div>
 
-        <div class="card" @click="() => goToProduct('https://buy.stripe.com/bIY8zY63oe691P2aEE', 'Alles!')">
-          <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
-          <div class="card-text">
-            <p>Alles!</p>
-            <p>Preis: $8</p>
-          </div>
-        </div>
       </div>
       <div v-if="activeTab === 'one-time'" class="tab-content">
-        <div class="card" @click="() => goToProduct('https://buy.stripe.com/aEUeYm8bwgehfFS8wz', '1 Keks')">
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/fZe2bA4Zk5zD9hueV0', '1 Keks')">
           <AvatarImage class='card-image' src="/keks.png" alt="Santa" />
           <div class="card-text">
             <p>1 Keks</p>
-            <p>Preis: $1</p>
+            <p>Preis: 1.-</p>
           </div>
         </div>
 
-        <div class="card" @click="() => goToProduct('https://buy.stripe.com/5kAeYmgI2gehaly146', '2 Kekse')">
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/bIY5nMezUbY1eBO9AF', '2 Kekse')">
           <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
           <div class="card-text">
             <p>2 Kekse</p>
-            <p>Preis: $2</p>
+            <p>Preis: 2.-</p>
           </div>
         </div>
 
-        <div class="card" @click="() => goToProduct('https://buy.stripe.com/4gwdUibnI5zD79mdQR', '1 Keks und eine Milch')">
+        <div class="card" @click="() => goToProduct('https://buy.stripe.com/8wM7vUbnI0fjfFSfZ2', '1 Keks und eine Milch')">
           <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
           <div class="card-text">
             <p>1 Keks und eine Milch</p>
-            <p>Preis: $5</p>
-          </div>
-        </div>
-
-        <div class="card" @click="() => goToProduct('https://buy.stripe.com/bIY8zY63oe691P2aEE', 'Alles!')">
-          <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
-          <div class="card-text">
-            <p>Alles!</p>
-            <p>Preis: $8</p>
+            <p>Preis: 5.-</p>
           </div>
         </div>
       </div>
@@ -173,5 +158,10 @@ const goToProduct = (url, productName) => {
 }
 .close-button{
   width: 100%;
+}
+
+.btn-switch{
+  background: hsl(var(--primary-foreground));
+  margin: 5px;
 }
 </style>
