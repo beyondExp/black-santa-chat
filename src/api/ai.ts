05969fa58cac2cc9181f0fa6005590cdb0348fbd
Lucas Bärlocher <lucas.baerlocher@olai.com>
2023-11-30 22:51:53 +0100

    add ft:gpt-3.5-turbo-1106:beyond-group-gmbh::8Qj9Th00

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -10,7 +10,7 @@ export async function sendPrompt(message: string) {
       "expertId": 5,
       "promptId": null,
       "conversationId": conversationId ? conversationId : null,
-      "model": "ft:gpt-3.5-turbo-1106:beyond-group-gmbh::8QahcMhO",
+      "model": "ft:gpt-3.5-turbo-1106:beyond-group-gmbh::8Qj9Th00",
       "message": message,
     };
 
