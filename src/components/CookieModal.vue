2d105d1d611d8645d58eb7966bd37ba9cd35147a
Lucas Bärlocher <lucas.baerlocher@olai.com>
2023-11-30 18:43:18 +0100

    Change cookiebutton to a Modal

--- src/components/CookieModal.vue
+++ src/components/CookieModal.vue
@@ -10,7 +10,8 @@ const toggleModal = () => {
   showModal.value = !showModal.value;
 }
 
-const goToProduct = (url) => {
+const goToProduct = (url, productName) => {
+  // You can use productName if needed for analytics or other purposes
   window.location.href = url;
 }
 </script>
@@ -25,39 +26,23 @@ const goToProduct = (url) => {
       <h1>Hinterlasse dem Santa einen Keks</h1>
       <p>Wähle aus, wie viele Kekse du dem Santa geben möchtest.</p>
 
-      <Button class="product" variant="primary" @click="() => goToProduct('https://buy.stripe.com/link1')">
-        <AvatarImage
-          class='logo-santa'
-          src="/keks.png"
-          alt="Santa"
-          size="55"
-          style="width: auto; height: 55px; margin: auto;"
-        />
-        1 Keks
-      </Button>
-      <Button class="product" variant="primary" @click="() => goToProduct('https://buy.stripe.com/link2')">
-        <AvatarImage
-          class='logo-santa'
-          src="/kekse.png"
-          alt="Santa"
-          size="55"
-          style="width: auto; height: 55px; margin: auto;"
-        />
-        2 Kekse
-      </Button>
-      <Button class="product" variant="primary" @click="() => goToProduct('https://buy.stripe.com/link3')">
-        <AvatarImage
-          class='logo-santa'
-          src="/kekse_mit_milch.png"
-          alt="Santa"
-          size="55"
-          style="width: auto; height: 55px; margin: auto;"
-        />
-        Keks mit Milch
-      </Button>
-      <Button class="product" variant="primary" @click="() => goToProduct('https://buy.stripe.com/link4')">
-        Alles!
-      </Button>
+      <div class="card" @click="() => goToProduct('https://buy.stripe.com/link1', '1 Keks')">
+        <AvatarImage class='card-image' src="/keks.png" alt="Santa" />
+        <div class="card-text">
+          <p>1 Keks</p>
+          <p>Preis: $5</p>
+        </div>
+      </div>
+
+      <div class="card" @click="() => goToProduct('https://buy.stripe.com/link2', '2 Kekse')">
+        <AvatarImage class='card-image' src="/kekse.png" alt="Santa" />
+        <div class="card-text">
+          <p>2 Kekse</p>
+          <p>Preis: $10</p>
+        </div>
+      </div>
+
+      <!-- Other cards -->
 
       <Button variant="secondary" @click="toggleModal">Close</Button>
     </div>
@@ -66,27 +51,35 @@ const goToProduct = (url) => {
 
 <style scoped>
 .modal {
-  position: fixed;
-  left: 0;
-  padding: 5px;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  margin-left: 0px !important;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.product{
-  width: 100% !important;
+  /* Existing styles */
 }
 
 .modal-content {
-  background: white;
-  padding: 20px;
-  border-radius: 5px;
+  /* Existing styles */
+}
+
+.card {
+  background: #f3f3f3;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
   text-align: center;
+}
+
+.card:hover {
+  background-color: #e9e9e9;
+}
+
+.card-image {
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  display: block;
+}
+
+.card-text p {
+  margin: 5px 0;
 }
 </style>
