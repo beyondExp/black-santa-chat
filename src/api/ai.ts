5c45d5b3ee57b0f34ff003f3e1b488528bd9e1b8
Lucas Bärlocher <lucas.baerlocher@olai.com>
2023-12-02 00:12:20 +0100

    updating llm to ft:gpt-3.5-turbo-1106:beyond-group-gmbh::8R6qKpxG

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -12,7 +12,7 @@ export async function sendPrompt(message: string) {
       "expertId": 5,
       "promptId": null,
       "conversationId": conversationId ? conversationId : null,
-      "model": "ft:gpt-3.5-turbo-1106:beyond-group-gmbh::8R6LjfFc",
+      "model": "ft:gpt-3.5-turbo-1106:beyond-group-gmbh::8R6qKpxG",
       "message": message,
     };
 
