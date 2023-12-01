7d2d37372c9999cfa7a3e36f6209b07dd44c49bf
Lucas Bärlocher <lucas.baerlocher@olai.com>
2023-12-01 22:59:07 +0100

    adding the render for markdown in the response

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -7,9 +7,23 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 mt-4 md:w-[35vw]">
+  <div class="flex flex-col gap-2 mt-4 md:w-[35vw] chat-message">
     <p class="leading-7">
       {{ message }}
     </p>
   </div>
 </template>
+<style>
+.chat-message >>> a{
+  background: #b11e31;
+  color: inherit;
+  text-decoration: inherit;
+  padding: 5px;
+  margin-top: 7px;
+  margin-bottom: 7px;
+  width: 100% !important;
+  display: block;
+  text-align: center;
+  border-radius: 5px;
+}
+</style>
