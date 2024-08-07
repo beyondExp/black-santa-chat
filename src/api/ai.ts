6c859ee0ba38aae8da81be2f60be76de713820b5
Lucas Bärlocher <lucas.baerlocher@olai.com>
2024-08-07 14:21:17 +0200

    updating for the new Demo Chat Feature

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -9,29 +9,40 @@ export async function sendPrompt(message: string) {
 
     // Prepare the payload for the POST request
     const payload = {
-      "expertId": 5,
-      "promptId": null,
-      "conversationId": conversationId ? conversationId : null,
-      "model": "ft:gpt-3.5-turbo-1106:beyond-group-gmbh::8R6qKpxG",
-      "message": message,
+      "expert_id": 37,
+      "ability_id": 80,
+      "model_id": 6 ,
+      "session_id": 6 ,
+      "user_id": 123,
+      "input_data": message,
+      "document_urls": [],
+      "tool_config": {
+        "tavily_max_results": 1
+      },
+      "tool_activation": {
+        "tavily_search": true,
+        "wolfram_alpha": true,
+        "document_retriever": false,
+        "notion_connector": false
+      }
     };
 
     const response = await axios.post(
-      "/api/expert/askBlackSanta",
+      "/api/v1/invoke_agent",
       payload,
       {
         headers: {
-          Authorization: `Bearer 3ZIB__S3hyi4VQjISGCL-6kxv5R-bphV`,
+          "bbot-api-key": `bbot_kvx97s1gkkx02mmx3jm0tpunjg7rohb1joy6985usz`,
         }
       }
     );
 
     // Save the data attribute to sessionStorage if it's not null
-    if (response.data.conversationId) {
-      sessionStorage.setItem('conversationId', response.data.conversationId);
-    }
+    //if (response.data.conversationId) {
+    //  sessionStorage.setItem('conversationId', response.data.conversationId);
+    //}
 
-    return markdown.render(response.data.result);
+    return markdown.render(response.data.output);
 
   } catch (error: any) {
     if (error.response && error.response.status === 500) {
